ff1c7f0596474d6b92b860f7a91f9b9a4ca7d71d
arturbazza <agencia@fonzie.com.br>
2024-04-10 16:26:41 -0300

    Cadastrando usuário

--- app/src/main/java/br/com/fonzie/sosplusseniorassistido/view/formcadastro/FormCadatro.kt
+++ app/src/main/java/br/com/fonzie/sosplusseniorassistido/view/formcadastro/FormCadatro.kt
@@ -6,10 +6,13 @@ import android.os.Bundle
 import br.com.fonzie.sosplusseniorassistido.R
 import br.com.fonzie.sosplusseniorassistido.databinding.ActivityFormCadatroBinding
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
 class FormCadatro : AppCompatActivity() {
 
     private lateinit var binding: ActivityFormCadatroBinding
+    private val auth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFormCadatroBinding.inflate((layoutInflater))
@@ -24,7 +27,17 @@ class FormCadatro : AppCompatActivity() {
                 snackbar.setBackgroundTint(Color.RED)
                 snackbar.show()
             } else {
+                auth.createUserWithEmailAndPassword(email, senha).addOnCompleteListener { cadastro ->
+                    if (cadastro.isSuccessful) {
+                        val snackbar = Snackbar.make(view,"Sucesso ao cadastrar usuário", Snackbar.LENGTH_SHORT)
+                        snackbar.setBackgroundTint(Color.BLUE)
+                        snackbar.show()
+                        binding.editEmail.setText("")
+                        binding.editSenha.setText("")
+                    }
+                }/*.addOnFailureListener {
 
+                }*/
             }
         }
     }
