878fa6dba6995d0c794de4dae5cef77c2d62ce92
arturbazza <agencia@fonzie.com.br>
2024-04-25 16:49:44 -0300

    Inserindo, Atualizando, lendo e deletando usuários. CRUD completo!

--- app/src/main/java/br/com/fonzie/sosplusseniorassistido/view/telaprincipal/TelaPrincipal.kt
+++ app/src/main/java/br/com/fonzie/sosplusseniorassistido/view/telaprincipal/TelaPrincipal.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import br.com.fonzie.sosplusseniorassistido.databinding.ActivityTelaPrincipalBinding
 import br.com.fonzie.sosplusseniorassistido.view.formlogin.FormLogin
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldPath
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 
@@ -29,14 +30,14 @@ class TelaPrincipal : AppCompatActivity() {
         binding.btGravarDadosDB.setOnClickListener {
 
             val usuariosMap = hashMapOf(
-                "nome" to "Ana",
-                "sobrenome" to "Silva",
-                "telefone" to 51987654321,
-//                "email" to "arturbazzanellamidia@gmail.com",
-//                "cpf" to 90329970097
+                "nome" to "Artur",
+                "sobrenome" to "Bazzanella",
+                "telefone" to 51999922078,
+                "email" to "arturbazzanellamidia@gmail.com",
+                "cpf" to 90329970097
             )
 
-            db.collection("Usuários").document("Ana")
+            db.collection("Usuários").document("Artur")
                 .set(usuariosMap).addOnCompleteListener {
                     Log.d("db", "Sucesso ao salvar os dados do usuário!")
                 }.addOnFailureListener {
@@ -44,24 +45,34 @@ class TelaPrincipal : AppCompatActivity() {
                 }
         }
         binding.btLerDadosBD.setOnClickListener {
-            db.collection("Usuários").document("Ana")
+            db.collection("Usuários").document("Artur")
                 .addSnapshotListener { documento, error ->
                     if (documento != null) {
                         var telefone = documento.getLong("telefone")
+                        var cpf = documento.getLong("cpf")
                         binding.txtResultadoNome.text = documento.getString("nome")
                         binding.txtResultadoSobrenome.text = documento.getString("sobrenome")
+                        binding.txtResultadoEmail.text = documento.getString("email")
                         binding.txtResultadoTelefone.text = telefone.toString()
+                        binding.txtResultadoCpf.text = cpf.toString()
                     }
                 }
         }
 
-//        binding.btAtualizarDadosDB.setOnClickListener {
-//
-//            db.collection("Usuários").document("Ana")
-//                .update("sobrenome", "da Silva").addOnCompleteListener {
-//                    Log.d("db_update", "Sucesso ao atualizar os dados do usuário!")
-//                }
-//        }
+        binding.btAtualizarDadosDB.setOnClickListener {
+
+            db.collection("Usuários").document("Artur")
+                .update("sobrenome", "da Silva", "telefone", 987654321).addOnCompleteListener {
+                    Log.d("db_update", "Sucesso ao atualizar os dados do usuário: ")
+                }
+        }
+
+        binding.btDeletarDadosBD.setOnClickListener {
+            db.collection("Usuários").document("Artur")
+                .delete().addOnCompleteListener {
+                    Log.d("db_delete", "Sucesso ao excluir os dados do usuário!")
+                }
+        }
 
     }
 }
